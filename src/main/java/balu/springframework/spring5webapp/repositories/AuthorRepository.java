2f336ee6b9ec7579c15e3869553733b113b6f2d8
Crassy <crassydeveloper@gmail.com>
2023-04-14 16:26:01 -0400

    Repositories Added.

---
+++ src/main/java/balu/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,7 @@
+package balu.springframework.spring5webapp.repositories;
+
+import balu.springframework.spring5webapp.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author,Long> {
+}
